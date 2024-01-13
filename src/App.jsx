c8aaf72beb09da4c78929444d517a3792d9662b0
masudmolla6 <masudmolla2937@gmail.com>
2024-01-13 17:25:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero/Hero"
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </div>
+  )
+}
+
+export default App
